fb26c392217f4d650ea44c533c756c326895513c
beckrecca <rebecca.doris42@gmail.com>
2016-06-22 21:00:37 -0400

    hidden input and info window on directions panel sandbox

---
+++ js/directions.js
@@ -0,0 +1,45 @@
+var directionsDisplay;
+var directionsService;
+var map;
+
+function initMap() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsService = new google.maps.DirectionsService();
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 42.3600825, lng: -71.05888010000001},
+    zoom: 11
+  });
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+  var marker = new google.maps.Marker({
+      position: {lat: parseFloat($('#lat').val()), lng: parseFloat($('#lng').val())},
+      map: map,
+      title: $('#title').val()
+    });
+  var infoContent = '<p>' + $('#title').val() + '<br/>' + $('#address').val(); + '</p>';
+  var infowindow = new google.maps.InfoWindow({
+    content: infoContent
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+  $('form').submit(function(e) {
+    e.preventDefault();
+    marker.setMap(null);
+    calcRoute();
+  });
+}
+function calcRoute() {
+  var start = $('#start').val();
+  var end = $('#address').val();
+  var request = {
+    origin:start,
+    destination:end,
+    travelMode: google.maps.TravelMode.TRANSIT
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  }); 
+} 
